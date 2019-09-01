5f8890a0c7f2ec1dca5001b8f1c5fc72ae1d6062
Thinh Cao <thinhdcao@outlook.com>
2019-09-01 15:12:57 -0400

    Initial commit

---
+++ Twilio_SMS/Model/TwilioAccount.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Twilio_SMS.Model
+{
+    class TwilioAccount
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+        public string FromNumber { get; set; }
+
+
+        /// <summary>
+        /// Get twilio's account sid and token from at text file
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void GetSidAndAuthToken(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadLines(filePath).ToArray();
+                AccountSid = lines[0];
+                AuthToken = lines[1];
+            }
+            catch (Exception)
+            {
+                ThrowConfigException();
+            }
+            
+        }
+
+        /// <summary>
+        /// Set the number where we will be sending sms from
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void SetFromNumber(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadLines(filePath).ToArray();
+                FromNumber = lines[2];
+            }
+            catch (Exception)
+            {
+                ThrowConfigException();
+            }
+        }
+
+        /// <summary>
+        /// Send message to entered phone number
+        /// </summary>
+        /// <param name="msgContent"></param>
+        /// <param name="toNumber"></param>
+        public void SendMessage(string msgContent, string toNumber)
+        {
+            string formattedToNumber = "+1" + toNumber;
+            StringBuilder summary = new StringBuilder("Summary: ");
+            summary.AppendLine("Message content: " + msgContent);
+            summary.AppendLine("Send to: " + formattedToNumber);
+            try
+            {
+                var message = MessageResource.Create(
+                    body: msgContent,
+                    from: new Twilio.Types.PhoneNumber(FromNumber),
+                    to: new Twilio.Types.PhoneNumber(formattedToNumber)
+                );
+                summary.AppendLine("Status: Successful!");
+            }
+            catch (Exception e)
+            {
+                summary.AppendLine("Status: Unsuccessful! See below for error");
+                summary.AppendLine(e.ToString());
+            }
+            Console.WriteLine(summary.ToString());
+        }
+
+        /// <summary>
+        /// Throw config file exception
+        /// </summary>
+        private void ThrowConfigException()
+        {
+            Console.WriteLine("Please make check your credential file!");
+            throw new Exception("Config file is in the wrong format!");
+        }
+    }
+}
---
+++ Twilio_SMS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio_SMS.Model;
+
+namespace Twilio_SMS
+{
+    class Program
+    {
+        // Substitute this with your Twilio account in the format:
+        // AccountSid
+        // AuthToken
+        // Twilio phone number
+        private const string FilePath = @"F:\Asp.NetProjects\Twilio_SMS\twilioAccount.txt"; 
+        static void Main(string[] args)
+        {
+            // Init a TwilioAccount object. Get Sid, authToken, and set fromNumber
+            TwilioAccount twilioAccount = new TwilioAccount();
+            twilioAccount.GetSidAndAuthToken(FilePath);
+            twilioAccount.SetFromNumber(FilePath);
+            TwilioClient.Init(twilioAccount.AccountSid, twilioAccount.AuthToken);
+
+            // Ask user for the number and message content
+            Console.WriteLine("Quick and dirty test of Twilio");
+            Console.WriteLine("Enter a message: ");
+            string msgContent = Console.ReadLine();
+            Console.WriteLine("Message entered: " + msgContent);
+            Console.WriteLine("Enter phone#: ");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Phone# entered: " + msgContent);
+
+            // Send message
+            twilioAccount.SendMessage(msgContent, phoneNumber);
+            
+            // Exit
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
